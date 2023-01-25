f54162b4907286977fa1a429328ed8732c1770b4
sahana <90238085+sahana3131@users.noreply.github.com>
2023-01-25 18:31:25 +0530

    Add files via upload

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject metabase_sftp_export 0.1.0-SNAPSHOT
+  description SFTP export plugin for Metabase
+  url httpsgithub.comsahana3131plugin
+  dependencies [[org.clojureclojure 1.10.1]]
+  main metabase_sftp_export.core
+  plugins [[lein-exec 0.3.7]]
+  )
